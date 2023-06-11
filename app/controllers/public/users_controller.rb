29a1c834392962061c29fad29eb3ee0bb24487b3
Marina Tsunoda <122651621+MarinaTsunoda@users.noreply.github.com>
2023-06-11 14:03:28 +0000

    Fix user/edit,my-page

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -26,9 +26,9 @@ class Public::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   private
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+    params.require(:user).permit(:user_name, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 end
